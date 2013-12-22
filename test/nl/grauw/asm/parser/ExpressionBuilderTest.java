8c9cf1dd109aca6c1cb0a7f509dad539a9724f60
Laurens Holst <laurens@grauw.nl>
2013-12-22 14:27:37 +0100

    ExpressionBuilder: Split up the tests.

--- test/nl/grauw/asm/parser/ExpressionBuilderTest.java
+++ test/nl/grauw/asm/parser/ExpressionBuilderTest.java
@@ -8,18 +8,62 @@ import org.junit.Test;
 public class ExpressionBuilderTest {
 
 	@Test
-	public void test() {
+	public void testSingleValue() {
 		assertEquals("a", parse("a"));
+	}
+	
+	@Test
+	public void testAddition() {
 		assertEquals("{a + 1H}", parse("a + 1H"));
+	}
+	
+	@Test
+	public void testDoubleAddition() {
 		assertEquals("{{a + 1H} + 2H}", parse("a + 1H + 2H"));
+	}
+	
+	@Test
+	public void testPrecedence() {
 		assertEquals("{a + {1H * 2H}}", parse("a + 1H * 2H"));
+	}
+	
+	@Test
+	public void testPrecedence2() {
 		assertEquals("{{a + {1H * 2H}} + b}", parse("a + 1H * 2H + b"));
+	}
+	
+	@Test
+	public void testGrouping() {
 		assertEquals("{a + {({1H + 2H}) * 3H}}", parse("a + (1H + 2H) * 3H"));
-		assertEquals("{a, {1H}}", parse("a, 1H"));
-		assertEquals("{{a + 1H}, {{b + 2H}}}", parse("a + 1H, b + 2H"));
-		assertEquals("{a, {{1H + 2H}, {{3H + 4H}}}}", parse("a, 1H + 2H, 3H + 4H"));
-		assertEquals("{1H + {({a, {2H, {3H}}}) * b}}", parse("1H + (a, 2H, 3H) * b"));
+	}
+	
+	@Test
+	public void testGrouping2() {
 		assertEquals("{{10H + {15H * ({5H - 2H})}} + 4H}", parse("10H + 15H * (5H - 2H) + 4H"));
+	}
+	
+	@Test
+	public void testSequence() {
+		assertEquals("{a, {1H}}", parse("a, 1H"));
+	}
+	
+	@Test
+	public void testSequence2() {
+		assertEquals("{{a + 1H}, {{b + 2H}}}", parse("a + 1H, b + 2H"));
+	}
+	
+	@Test
+	public void testSequence3() {
+		assertEquals("{a, {{1H + 2H}, {{3H + 4H}}}}", parse("a, 1H + 2H, 3H + 4H"));
+	}
+	
+	@Test
+	public void testSequenceInGroup() {
+		assertEquals("{1H + {({a, {2H, {3H}}}) * b}}", parse("1H + (a, 2H, 3H) * b"));
+	}
+	
+	@Test
+	public void testSequenceWithDoubleGroup() {
 		assertEquals("{a, {{{({10H + 15H}) * ({5H - 2H})} + 4H}}}", parse("a, (10H + 15H) * (5H - 2H) + 4H"));
 	}
 	
