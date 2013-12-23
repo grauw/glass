645a39e496c7443cc8e5010bbc9f78d650912acf
Laurens Holst <laurens@grauw.nl>
2013-12-23 12:54:09 +0100

    ArgumentException: Add parameterless constructor.

--- src/nl/grauw/asm/instructions/ArgumentException.java
+++ src/nl/grauw/asm/instructions/ArgumentException.java
@@ -3,6 +3,10 @@ package nl.grauw.asm.instructions;
 public class ArgumentException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 	
+	public ArgumentException() {
+		this("Invalid arguments.");
+	}
+	
 	public ArgumentException(String message) {
 		super(message);
 	}
