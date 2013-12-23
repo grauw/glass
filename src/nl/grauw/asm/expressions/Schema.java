14c6b6b8af3187a3c3ea58312aca9cdfdd303cd2
Laurens Holst <laurens@grauw.nl>
2013-12-23 23:39:09 +0100

    Schema: Add a bunch of types.

--- src/nl/grauw/asm/expressions/Schema.java
+++ src/nl/grauw/asm/expressions/Schema.java
@@ -28,4 +28,62 @@ public class Schema {
 		}
 	}
 	
+	public static Type DIRECT_INT = new DirectInteger();
+	public static class DirectInteger extends Type {
+		public boolean check(Expression argument) {
+			return !(argument instanceof Group) && argument.isInteger();
+		}
+	}
+	
+	public static Type DIRECT_A = new DirectA();
+	public static class DirectA extends Type {
+		public boolean check(Expression argument) {
+			return !(argument instanceof Group) && argument.isRegister() && argument.getRegister() == Register.A;
+		}
+	}
+	
+	public static Type DIRECT_HL = new DirectHL();
+	public static class DirectHL extends Type {
+		public boolean check(Expression argument) {
+			return !(argument instanceof Group) && argument.isRegister() && argument.getRegister() == Register.HL;
+		}
+	}
+	
+	public static Type DIRECT_HL_IX_IY = new DirectHLIXIY();
+	public static class DirectHLIXIY extends Type {
+		public boolean check(Expression argument) {
+			if (!(argument instanceof Group) && argument.isRegister()) {
+				Register register = argument.getRegister();
+				return register == Register.HL || register == Register.IX || register == Register.IY;
+			}
+			return false;
+		}
+	}
+	
+	public static Type INDIRECT_INT = new IndirectInteger();
+	public static class IndirectInteger extends Type {
+		public boolean check(Expression argument) {
+			return argument instanceof Group && argument.isInteger();
+		}
+	}
+	
+	public static Type INDIRECT_C = new IndirectC();
+	public static class IndirectC extends Type {
+		public boolean check(Expression argument) {
+			return argument instanceof Group && argument.isRegister() && argument.getRegister().isPair();
+		}
+	}
+	
+	public static Type DIRECT_A_INDIRECT_HL_IX_IY = new DirectAIndirectHLIXIY();
+	public static class DirectAIndirectHLIXIY extends Type {
+		public boolean check(Expression argument) {
+			if (argument.isRegister()) {
+				Register register = argument.getRegister();
+				return !(argument instanceof Group) && !register.isPair() ||
+						argument instanceof Group && (register == Register.HL || register.isIndex());
+			}
+			return false;
+		}
+	}
+	
 }
