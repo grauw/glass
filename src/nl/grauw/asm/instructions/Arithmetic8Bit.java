5789aa7db8d949686cf0293712a27fea94a26ddd
Laurens Holst <laurens@grauw.nl>
2013-12-23 19:47:06 +0100

    Arithmetic8Bit: Simplify.

--- src/nl/grauw/asm/instructions/Arithmetic8Bit.java
+++ src/nl/grauw/asm/instructions/Arithmetic8Bit.java
@@ -1,12 +1,9 @@
 package nl.grauw.asm.instructions;
 
-import nl.grauw.asm.expressions.Add;
 import nl.grauw.asm.expressions.Expression;
 import nl.grauw.asm.expressions.Group;
-import nl.grauw.asm.expressions.IntegerLiteral;
 import nl.grauw.asm.expressions.Register;
 import nl.grauw.asm.expressions.Sequence;
-import nl.grauw.asm.expressions.Subtract;
 
 public abstract class Arithmetic8Bit extends Instruction {
 	
@@ -23,78 +20,24 @@ public abstract class Arithmetic8Bit extends Instruction {
 	
 	@Override
 	public byte[] getBytes() {
-		if (argument instanceof IntegerLiteral) {
-			int value = ((IntegerLiteral)argument).getValue();
+		if (argument.isInteger()) {
+			int value = argument.evaluateInteger();
 			return new byte[] { (byte)(0xC6 | getMask()), (byte)value };
-		} else {
-			Register register = getRegister(argument);
-			if (register != null) {
-				if (!register.isPair()) {
-					if (!register.isIndex())
-						return new byte[] { (byte)(0x80 | getMask() | register.getCode()) };
-					else
-						return new byte[] { register.getIndexCode(), (byte)(0x80 | getMask() | register.getCode()) };
-				} else {
-					if (!register.isIndex())
-						return new byte[] { (byte)(0x80 | getMask() | 6) };  // TODO magic constant
-					else
-						return new byte[] { register.getIndexCode(), (byte)(0x80 | getMask() | 6), getOffset(argument) };
-				}
+		} else if (argument.isRegister()) {
+			Register register = argument.evaluateRegister();
+			if (!register.isPair()) {
+				if (!register.isIndex())
+					return new byte[] { (byte)(0x80 | getMask() | register.getCode()) };
+				else
+					return new byte[] { register.getIndexCode(), (byte)(0x80 | getMask() | register.getCode()) };
+			} else if (argument instanceof Group && (register == Register.HL || register.isIndex())) {
+				if (!register.isIndex())
+					return new byte[] { (byte)(0x80 | getMask() | 6) };  // TODO magic constant
+				else
+					return new byte[] { register.getIndexCode(), (byte)(0x80 | getMask() | 6), (byte)register.getOffset() };
 			}
 		}
 		throw new ArgumentException();
-	}
-	
-	private Register getRegister(Expression argument) {
-		if (argument instanceof Register) {
-			Register register = (Register)argument;
-			if (!register.isPair())
-				return register;
-		} else if (argument instanceof Group) {
-			Expression contents = ((Group)argument).getTerm();
-			if (contents == Register.HL) {
-				return Register.HL;
-			} else if (contents instanceof Add) {
-				Add addition = (Add)contents;
-				if (addition.getAugend() instanceof Register) {
-					if (addition.getAugend() == Register.IX)
-						return Register.IX;
-					else if (addition.getAugend() == Register.IY)
-						return Register.IY;
-				}
-			} else if (contents instanceof Subtract) {
-				Subtract subtraction = (Subtract)contents;
-				if (subtraction.getMinuend() instanceof Register) {
-					if (subtraction.getMinuend() == Register.IX)
-						return Register.IX;
-					else if (subtraction.getMinuend() == Register.IY)
-						return Register.IY;
-				}
-			}
-		}
-		throw new ArgumentException("Not a register.");
-	}
-	
-	private byte getOffset(Expression argument) {
-		Expression contents = ((Group)argument).getTerm();
-		if (contents instanceof Add) {
-			Expression addend = ((Add)contents).getAddend();
-			if (addend instanceof IntegerLiteral) {
-				int offset = ((IntegerLiteral)addend).getValue();
-				if (offset < -128 || offset > 127)
-					throw new ArgumentException("Offset out of range.");
-				return (byte)offset;
-			}
-		} else if (contents instanceof Subtract) {
-			Expression subtrahend = ((Subtract)contents).getSubtrahend();
-			if (subtrahend instanceof IntegerLiteral) {
-				int offset = -((IntegerLiteral)subtrahend).getValue();
-				if (offset < -128 || offset > 127)
-					throw new ArgumentException("Offset out of range.");
-				return (byte)offset;
-			}
-		}
-		throw new ArgumentException("Not an offset.");
 	}
 	
 }
