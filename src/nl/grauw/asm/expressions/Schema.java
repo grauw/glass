c3464ba932be870de03059743e552ae430fb546b
Laurens Holst <laurens@grauw.nl>
2013-12-24 15:08:45 +0100

    Schema: Also implement SchemaType itself.
    (It is a sequence matcher.)

--- src/nl/grauw/asm/expressions/Schema.java
+++ src/nl/grauw/asm/expressions/Schema.java
@@ -1,10 +1,10 @@
 package nl.grauw.asm.expressions;
 
-public class Schema {
+public class Schema implements SchemaType {
 	
-	private Type[] types;
+	private SchemaType[] types;
 	
-	public Schema(Type... types) {
+	public Schema(SchemaType... types) {
 		this.types = types;
 	}
 	
@@ -17,66 +17,62 @@ public class Schema {
 		return arguments == null;
 	}
 	
-	public static Type ANY = new Any();
-	public static Type DIRECT = new Direct();
-	public static Type INDIRECT = new Indirect();
-	public static Type INTEGER = new Integer();
-	public static Type DIRECT_INT = new And(DIRECT, INTEGER);
-	public static Type DIRECT_A = new And(DIRECT, new Reg(Register.A));
-	public static Type DIRECT_RR = new And(DIRECT, new Reg(Register.BC, Register.DE, Register.HL, Register.SP));
-	public static Type DIRECT_RR_INDEX = new And(DIRECT, new Reg(Register.BC, Register.DE, Register.HL, Register.SP, Register.IX, Register.IY));
-	public static Type DIRECT_DE = new And(DIRECT, new Reg(Register.DE));
-	public static Type DIRECT_HL = new And(DIRECT, new Reg(Register.HL));
-	public static Type DIRECT_HL_IX_IY = new And(DIRECT, new Reg(Register.HL, Register.IX, Register.IY));
-	public static Type DIRECT_AF = new And(DIRECT, new Reg(Register.AF));
-	public static Type DIRECT_AF_ = new And(DIRECT, new Reg(Register.AF_));
-	public static Type INDIRECT_INT = new And(INDIRECT, INTEGER);
-	public static Type INDIRECT_C = new And(INDIRECT, new Reg(Register.C));
-	public static Type INDIRECT_SP = new And(INDIRECT, new Reg(Register.SP));
-	public static Type DIRECT_R_INDIRECT_HL_IX_IY = new DirectRIndirectHLIXIY();
+	public static SchemaType ANY = new Any();
+	public static SchemaType DIRECT = new Direct();
+	public static SchemaType INDIRECT = new Indirect();
+	public static SchemaType INTEGER = new Integer();
+	public static SchemaType DIRECT_INT = new And(DIRECT, INTEGER);
+	public static SchemaType DIRECT_A = new And(DIRECT, new Reg(Register.A));
+	public static SchemaType DIRECT_RR = new And(DIRECT, new Reg(Register.BC, Register.DE, Register.HL, Register.SP));
+	public static SchemaType DIRECT_RR_INDEX = new And(DIRECT, new Reg(Register.BC, Register.DE, Register.HL, Register.SP, Register.IX, Register.IY));
+	public static SchemaType DIRECT_DE = new And(DIRECT, new Reg(Register.DE));
+	public static SchemaType DIRECT_HL = new And(DIRECT, new Reg(Register.HL));
+	public static SchemaType DIRECT_HL_IX_IY = new And(DIRECT, new Reg(Register.HL, Register.IX, Register.IY));
+	public static SchemaType DIRECT_AF = new And(DIRECT, new Reg(Register.AF));
+	public static SchemaType DIRECT_AF_ = new And(DIRECT, new Reg(Register.AF_));
+	public static SchemaType INDIRECT_INT = new And(INDIRECT, INTEGER);
+	public static SchemaType INDIRECT_C = new And(INDIRECT, new Reg(Register.C));
+	public static SchemaType INDIRECT_SP = new And(INDIRECT, new Reg(Register.SP));
+	public static SchemaType DIRECT_R_INDIRECT_HL_IX_IY = new DirectRIndirectHLIXIY();
 	
-	public abstract static class Type {
-		public abstract boolean check(Expression argument);
-	}
-	
-	public static class Any extends Type {
+	public static class Any implements SchemaType {
 		public boolean check(Expression argument) {
 			return true;
 		}
 	}
 	
-	public static class And extends Type {
-		private Type[] types;
-		public And(Type... types) {
+	public static class And implements SchemaType {
+		private SchemaType[] types;
+		public And(SchemaType... types) {
 			this.types = types;
 		}
 		public boolean check(Expression argument) {
-			for (Type type : types)
+			for (SchemaType type : types)
 				if (!type.check(argument))
 					return false;
 			return true;
 		}
 	}
 	
-	public static class Direct extends Type {
+	public static class Direct implements SchemaType {
 		public boolean check(Expression argument) {
 			return !(argument instanceof Group);
 		}
 	}
 	
-	public static class Indirect extends Type {
+	public static class Indirect implements SchemaType {
 		public boolean check(Expression argument) {
 			return argument instanceof Group;
 		}
 	}
 	
-	public static class Integer extends Type {
+	public static class Integer implements SchemaType {
 		public boolean check(Expression argument) {
 			return argument.isInteger();
 		}
 	}
 	
-	public static class Reg extends Type {
+	public static class Reg implements SchemaType {
 		private Register[] registers;
 		public Reg(Register... registers) {
 			this.registers = registers;
@@ -92,7 +88,7 @@ public class Schema {
 		}
 	}
 	
-	public static class DirectRIndirectHLIXIY extends Type {
+	public static class DirectRIndirectHLIXIY implements SchemaType {
 		public boolean check(Expression argument) {
 			if (argument.isRegister()) {
 				Register register = argument.getRegister();
---
+++ src/nl/grauw/asm/expressions/SchemaType.java
@@ -0,0 +1,5 @@
+package nl.grauw.asm.expressions;
+
+public interface SchemaType {
+	public abstract boolean check(Expression argument);
+}
