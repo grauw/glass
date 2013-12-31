0db3f78e32016256cd7a5fb60c21e31d65cb0332
Laurens Holst <laurens@grauw.nl>
2013-12-31 15:08:07 +0100

    Macro: Import MacroInstruction.

--- src/nl/grauw/glass/directives/Macro.java
+++ src/nl/grauw/glass/directives/Macro.java
@@ -4,6 +4,7 @@ import nl.grauw.glass.AssemblyException;
 import nl.grauw.glass.Line;
 import nl.grauw.glass.Scope;
 import nl.grauw.glass.Source;
+import nl.grauw.glass.instructions.MacroInstruction;
 
 public class Macro extends Directive {
 	
@@ -17,7 +18,7 @@ public class Macro extends Directive {
 	public void register(Scope scope, Line line) {
 		if (line.getLabel() == null)
 			throw new AssemblyException("Macro without label.");
-		scope.addInstruction(line.getLabel(), new nl.grauw.glass.instructions.MacroInstruction(line.getArguments(), source));
+		scope.addInstruction(line.getLabel(), new MacroInstruction(line.getArguments(), source));
 		super.register(scope, line);
 	}
 	
