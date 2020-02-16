42bce62fb8276e143c8596155f5ddb88a52e216b
Laurens Holst <laurens@grauw.nl>
2020-02-16 21:49:44 +0100

    SymbolsTest: Add test for symbol file generation.

---
+++ src/test/java/nl/grauw/glass/SymbolsTest.java
@@ -0,0 +1,218 @@
+package nl.grauw.glass;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+public class SymbolsTest {
+
+	@Test
+	public void testNone() {
+		assertIterableEquals(
+			s(),
+			symbols(
+				" nop"
+			)
+		);
+	}
+
+	@Test
+	public void testInstruction() {
+		assertIterableEquals(
+			s(
+				"test: equ 0H",
+				"test2: equ 1H"
+			),
+			symbols(
+				"test: nop",
+				"test2: nop"
+			)
+		);
+	}
+
+	@Test
+	public void testEqu() {
+		assertIterableEquals(
+			s(),
+			symbols(
+				"test: equ 10H",
+				"test2: equ 20H"
+			)
+		);
+	}
+
+	@Test
+	public void testMacro() {
+		assertIterableEquals(
+			s(
+				"test: equ 1H",
+				"test._size: equ 6H",
+				"test.x: equ 1H",
+				"test.y: equ 5H"
+			),
+			symbols(
+				" nop",
+				"Test: MACRO",
+				"x: dd -1",
+				"y: db -1",
+				"_size: ENDM",
+				"test: Test"
+			)
+		);
+	}
+
+	@Test
+	public void testProc() {
+		assertIterableEquals(
+			s(
+				"Test: equ 1H",
+				"Test.x: equ 1H",
+				"Test.y: equ 3H",
+				"Test.z: equ 5H"
+			),
+			symbols(
+				" nop",
+				"Test: PROC",
+				"x: push ix",
+				"y: pop ix",
+				"z: ENDP"
+			)
+		);
+	}
+
+	@Test
+	public void testRept() {
+		assertIterableEquals(
+			s(
+				"Test: equ 1H",
+				"Test.0: equ 1H",
+				"Test.0.x: equ 1H",
+				"Test.0.y: equ 3H",
+				"Test.1: equ 5H",
+				"Test.1.x: equ 5H",
+				"Test.1.y: equ 7H"
+			),
+			symbols(
+				" nop",
+				"Test: REPT 2, ?value",
+				"x: push ix",
+				"y: pop ix",
+				"z: equ ?value",
+				" ENDM"
+			)
+		);
+	}
+
+	@Test
+	public void testIrp() {
+		assertIterableEquals(
+			s(
+				"Test: equ 1H",
+				"Test.0: equ 1H",
+				"Test.0.x: equ 1H",
+				"Test.0.y: equ 3H",
+				"Test.1: equ 5H",
+				"Test.1.x: equ 5H",
+				"Test.1.y: equ 7H"
+			),
+			symbols(
+				" nop",
+				"Test: IRP ?value, 5, 7",
+				"x: push ix",
+				"y: pop ix",
+				"z: equ ?value",
+				" ENDM"
+			)
+		);
+	}
+
+	@Test
+	public void testIf() {
+		assertIterableEquals(
+			s(
+				"Test: equ 1H",
+				"x: equ 1H",
+				"z: equ 4H"
+			),
+			symbols(
+				" nop",
+				"Test: IF 1",
+				"x: ld hl,$",
+				"z: ELSE",
+				"y: pop ix",
+				"z: ENDIF"
+			)
+		);
+	}
+
+	@Test
+	public void testSection() {
+		assertIterableEquals(
+			s(
+				"ROM: equ 1H",
+				"x: equ 1H",
+				"y: equ 4H",
+				"z: equ 9H"
+			),
+			symbols(
+				" nop",
+				"ROM: ds 8H",
+				"z: nop",
+				" SECTION ROM",
+				"x: ld hl,$",
+				"y: ld hl,$",
+				" ENDS"
+			)
+		);
+	}
+
+	@Test
+	public void testInclude() throws IOException {
+		Files.write(temporaryDirectory.resolve("testInclude.asm"), Arrays.asList(
+			"test2: push ix",
+			"test3: pop ix"
+		));
+
+		assertIterableEquals(
+			s(
+				"test1: equ 1H",
+				"test2: equ 1H",
+				"test3: equ 3H",
+				"test4: equ 5H"
+			),
+			symbols(
+				" nop",
+				"test1: include \"testInclude.asm\"",
+				"test4:"
+			)
+		);
+	}
+
+	@TempDir
+	static Path temporaryDirectory;
+
+	public List<String> symbols(String... sourceLines) {
+		SourceBuilder sourceBuilder = new SourceBuilder(Arrays.asList(temporaryDirectory));
+		Source source = sourceBuilder.parse(new SourceFile(String.join("\n", sourceLines)));
+		try {
+			source.assemble(new Assembler.NullOutputStream());
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		String symbols = source.getScope().serializeSymbols();
+		return symbols.isEmpty() ? Collections.emptyList() : Arrays.asList(symbols.split("\\R"));
+	}
+
+	public List<String> s(String... lines) {
+		return Arrays.asList(lines);
+	}
+
+}
